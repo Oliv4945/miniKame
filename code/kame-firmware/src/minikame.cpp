631bbee575198019bf4d50b905ad6739692dc25b
Oliv4945 <oliv4945@gmail.com>
2017-03-01 14:46:41 +0100

    Change servo pinout to home made wemos servo shield

--- code/kame-firmware/src/minikame.cpp
+++ code/kame-firmware/src/minikame.cpp
@@ -3,14 +3,14 @@
 
 void MiniKame::init(){
     // Map between servos and board pins
-    board_pins[0] = D1; // Servo S0
-    board_pins[1] = D4; // Servo S1
-    board_pins[2] = D8; // Servo S2
-    board_pins[3] = D6; // Servo S3
-    board_pins[4] = D7; // Servo S4
-    board_pins[5] = D5; // Servo S5
-    board_pins[6] = D2; // Servo S6
-    board_pins[7] = D3; // Servo S7
+    board_pins[0] = D4; // Servo S0 - FLI
+    board_pins[1] = D2; // Servo S1 - FRI
+    board_pins[2] = D3; // Servo S2 - FLO
+    board_pins[3] = D1; // Servo S3 - FRO
+    board_pins[4] = D8; // Servo S4 - BLI
+    board_pins[5] = D6; // Servo S5 - BRI
+    board_pins[6] = D7; // Servo S6 - BLO
+    board_pins[7] = D5; // Servo S7 - BRO
 
     // Trim values for zero position calibration.
     trim[0] = 0;
