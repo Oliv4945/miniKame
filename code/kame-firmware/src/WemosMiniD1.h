6962cb2081496e879e8d7b2b03afe4a707cd08ce
Oliv4945 <oliv4945@gmail.com>
2017-03-02 12:03:37 +0100

    Add Wemos Mini D1 pin definition

---
+++ code/kame-firmware/src/WemosMiniD1.h
@@ -0,0 +1,18 @@
+#ifndef wemos_mini_d1_h
+#define wemos_mini_d1_h
+  #include <Arduino.h>
+  
+  // Define Wemos pin corresponding to Arduino
+  const unsigned int W_D0 = 16;
+  const unsigned int W_D1 = 5;
+  const unsigned int W_D2 = 4;
+  const unsigned int W_D3 = 0;
+  const unsigned int W_D4 = 2;
+  const unsigned int W_D5 = 14;
+  const unsigned int W_D6 = 12;
+  const unsigned int W_D7 = 13;
+  const unsigned int W_D8 = 15;
+  const unsigned int W_A0 = A0;
+  const unsigned int W_RX = 3;
+  const unsigned int W_TX = 1;
+#endif
