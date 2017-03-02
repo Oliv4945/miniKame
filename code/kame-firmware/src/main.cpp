be3a640b9caa842a9630305ae5dea6e2cdf25ebb
Oliv4945 <oliv4945@gmail.com>
2017-03-02 13:34:58 +0100

    Webserver basics

--- code/kame-firmware/src/main.cpp
+++ code/kame-firmware/src/main.cpp
@@ -1,6 +1,7 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include <Servo.h>
 #include "WemosMiniD1.h"
 #include "minikame.h"
@@ -11,17 +12,17 @@
 const char* ssid = "kame";
 const char* password = "asdf";
 
-void parseData(String data);
 
 MiniKame robot( /* FLI */ W_D4, /* FRI */ W_D2,
                 /* FLO */ W_D3, /* FR0 */ W_D1,
                 /* BLI */ W_D8, /* BRI */ W_D6,
                 /* BLO */ W_D7, /* BRO */ W_D5
               );
-WiFiServer server(80);
-bool running=0;
-String input;
+ESP8266WebServer server(80);
 
+// Declare functions
+void handleRoot();
+void handleNotFound();
 
 void setup() {
     WiFi.mode(WIFI_AP);
@@ -29,11 +30,36 @@ void setup() {
     server.begin();
     Serial.begin(115200);
 
+    Serial.print("MiniKame starting...");
     delay(1000);
     robot.init();
+    Serial.println("OK");
+
+
+    Serial.print("IP - ");
+    Serial.println(WiFi.localIP());
+    Serial.print("MDNS - ");
+    if (MDNS.begin("kame")) {
+      Serial.println("OK");
+    } else {
+      Serial.println("NOK");
+    }
+
+    // Define web pages callbacks
+    server.on("/", handleRoot);
+    server.on("/inline", [](){
+      server.send(200, "text/plain", "this works as well");
+    });
+    server.onNotFound(handleNotFound);
+
+    server.begin();
+    Serial.println("HTTP - Server started\n");
 }
 
 void loop() {
+    server.handleClient();
+
+    /*
     WiFiClient client = server.available();
     if (!client) {
         IPAddress myIP = WiFi.softAPIP();
@@ -62,8 +88,10 @@ void loop() {
             else robot.home();
         }
     }
+    */
 }
 
+/*
 void parseData(String data){
 
     switch (data.toInt()){
@@ -118,4 +146,24 @@ void parseData(String data){
             robot.home();
             break;
     }
+}
+*/
+
+
+void handleRoot() {
+  server.send(200, "text/plain", "kame control");}
+
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
 }
